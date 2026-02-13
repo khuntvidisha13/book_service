cf9627b98b5f89ed769ee7f1b81382b32bb9ef5b
khuntvidisha13 <155361544+khuntvidisha13@users.noreply.github.com>
2026-02-13 11:05:05 +0530

    Add files via upload

---
+++ HomeEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeEase.HomeEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
